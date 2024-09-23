b329283eb08c9b303a44ac342977254887a054aa
belzevol <belzevol@gmail.com>
2024-09-24 01:15:04 +0300

    fix task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,6 @@
 console.log("TASK-3");
 const sortByDescendingFriendCount = (users) =>
-  users.sort((a, b) => b.friends.length - a.friends.length);
-
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 console.log(
   sortByDescendingFriendCount([
     {
